6a9cd95b36d61cb155d11b939a8a8a8c98f805ee
Joshua Smith <smithjoshua001@aol.co.uk>
2016-02-24 13:29:01 +0000

    fixed model and setupbash file for multiple distros and navigation

--- first_setup.bash
+++ first_setup.bash
@@ -1,6 +1,5 @@
 git submodule init
 git submodule update
 sudo apt-get update
-sudo -H apt-get install -y libusb-dev libspnav-dev ros-indigo-move-base libaria-dev
-rosdep install rosaria
+sudo -H -E apt-get install -y libusb-dev libspnav-dev ros-$ROS_DISTRO-navigation ros-$ROS_DISTRO-openni-launch libaria-dev
 
